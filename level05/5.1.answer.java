c9c1146865602625e7c825349186e853a18728cf
leadenhead <plaga@mail.ru>
2016-08-04 23:52:03 +0000

    add 5.2

---
+++ level05/5.1.answer.java
@@ -0,0 +1,12 @@
+
+/* Создать класс Cat
+Создать класс Cat. У кота должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).
+*/
+
+public class Cat
+{
+    String name;
+    int age;
+    int weight;
+    int strength;
+}
