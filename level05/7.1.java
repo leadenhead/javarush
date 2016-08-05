5e44cc491f4f09e7a9eaa7ee706fb5e44f3719a2
leadenhead <plaga@mail.ru>
2016-08-05 02:37:49 +0000

    add task

---
+++ level05/7.1.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level05.lesson07.task01;
+
+/* Создать класс Friend
+Создать класс Friend (друг) с тремя инициализаторами (тремя методами initialize):
+- Имя
+- Имя, возраст
+- Имя, возраст, пол
+*/
+
+public class Friend
+{
+    //напишите тут ваш код
+}
