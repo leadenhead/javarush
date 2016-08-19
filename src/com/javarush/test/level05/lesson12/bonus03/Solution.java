edea7ae81ac549bba689428010a5119d77c23031
Stoned Astronaut <stoned.astronaut@ya.ru>
2016-08-19 11:25:43 +0700

    fix all this

---
+++ src/com/javarush/test/level05/lesson12/bonus03/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.test.level05.lesson12.bonus03;
+
+import java.io.*;
+
+/* Задача по алгоритмам
+Написать программу, которая:
+1. вводит с консоли число N > 0
+2. потом вводит N чисел с консоли
+3. выводит на экран максимальное из введенных N чисел.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        // int maximum = ;
+        int N = Integer.parseInt(reader.readLine());
+        int[] arr = new int[N];
+
+        //напишите тут ваш код
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(reader.readLine());
+
+        int maximum = compare(arr);
+
+        System.out.println(maximum);
+    }
+
+    static int compare(int[] array) {
+        int max = 0;
+
+        for (int i = 1; i <= array.length; i++) {
+            for (int j = i - 1; j < array.length; j++) {
+                if (array[i] < array[j])
+                    max = array[j];
+            }
+        }
+
+        return max;
+    }
+}
