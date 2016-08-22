3a16eff710d1528d82c3d5fa8a7fc503c5325f98
leadenhead <plaga@mail.ru>
2016-08-22 19:09:56 +0000

    add level 6 lesson 11

---
+++ src/com/javarush/test/level06/lesson11/home07/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level06.lesson11.home07;
+
+import java.util.ArrayList;
+
+/* Три статические переменных name
+Создай 3 public статических переменных: String Solution.name, String Cat.name, String Dog.name
+*/
+
+public class Solution
+{
+    public static class Cat {
+    }
+
+    public static class Dog {
+    }
+}
