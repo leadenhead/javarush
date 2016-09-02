bf7132429adfbc1618092a64c5c1656377f18437
leadenhead <plaga@mail.ru>
2016-09-02 02:41:18 +0000

    add level 8 (all lesson)

---
+++ src/com/javarush/test/level08/lesson08/task03/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.test.level08.lesson08.task03;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/* Одинаковые имя и фамилия
+Создать словарь (Map<String, String>) занести в него десять записей по принципу «Фамилия» - «Имя».
+Проверить сколько людей имеют совпадающие с заданным имя или фамилию.
+*/
+
+public class Solution
+{
+    public static HashMap<String, String> createMap()
+    {
+        //напишите тут ваш код
+
+    }
+
+    public static int getCountTheSameFirstName(HashMap<String, String> map, String name)
+    {
+        //напишите тут ваш код
+
+    }
+
+    public static int getCountTheSameLastName(HashMap<String, String> map, String lastName)
+    {
+        //напишите тут ваш код
+
+    }
+}
---
+++ src/com/javarush/test/level08/lesson08/task05/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.test.level08.lesson08.task05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* Удалить людей, имеющих одинаковые имена
+Создать словарь (Map<String, String>) занести в него десять записей по принципу «фамилия» - «имя».
+Удалить людей, имеющих одинаковые имена.
+*/
+
+public class Solution
+{
+    public static HashMap<String, String> createMap()
+    {
+        //напишите тут ваш код
+
+    }
+
+    public static void removeTheFirstNameDuplicates(HashMap<String, String> map)
+    {
+        //напишите тут ваш код
+
+    }
+
+    public static void removeItemFromMapByValue(HashMap<String, String> map, String value)
+    {
+        HashMap<String, String> copy = new HashMap<String, String>(map);
+        for (Map.Entry<String, String> pair: copy.entrySet())
+        {
+            if (pair.getValue().equals(value))
+                map.remove(pair.getKey());
+        }
+    }
+}
