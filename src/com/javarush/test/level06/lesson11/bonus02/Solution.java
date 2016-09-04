b1af80f716c3feddbe47cfe76ca29e6e234b9d41
Sergey Vlasov <madk1nd@yandex.ru>
2016-09-04 11:08:14 +0200

    Add solution (I did not read the question carefully) level06-lesson11-bonus02

--- src/com/javarush/test/level06/lesson11/bonus02/Solution.java
+++ src/com/javarush/test/level06/lesson11/bonus02/Solution.java
@@ -33,24 +33,23 @@ public class Solution
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        String grandFather = reader.readLine();
-        Cat catGrandFather = new Cat(grandFather);
+        String grandFatherName = reader.readLine();
+        Cat catGrandFather = new Cat(grandFatherName);
 
-        String grandMother = reader.readLine();
-        Cat catGrandMother = new Cat(grandMother);
+        String grandMotherName = reader.readLine();
+        Cat catGrandMother = new Cat(grandMotherName);
 
-        String father = reader.readLine();
-        Cat catFather = new Cat(father, catGrandFather);
-        catFather.thisIsDad();
+        String fatherName = reader.readLine();
+        Cat catFather = new Cat(fatherName, catGrandFather, null);
 
-        String mother = reader.readLine();
-        Cat catMother = new Cat(mother, catGrandMother);
+        String motherName = reader.readLine();
+        Cat catMother = new Cat(motherName, null, catGrandMother);
 
-        String sun = reader.readLine();
-        Cat catSun = new Cat(sun, catMother, catFather);
+        String sunName = reader.readLine();
+        Cat catSun = new Cat(sunName, catFather, catMother);
 
-        String daughter = reader.readLine();
-        Cat catDaughter = new Cat(daughter, catMother, catFather);
+        String daughterName = reader.readLine();
+        Cat catDaughter = new Cat(daughterName, catFather, catMother);
 
         System.out.println(catGrandFather);
         System.out.println(catGrandMother);
@@ -63,43 +62,32 @@ public class Solution
     public static class Cat
     {
         private String name;
-        private Cat parent;
-        private Cat parent2;
+        private Cat father;
+        private Cat mother;
 
         Cat(String name)
         {
             this.name = name;
         }
 
-        Cat(String name, Cat parent)
-        {
+        public Cat(String name, Cat father, Cat mother) {
             this.name = name;
-            this.parent = parent;
-        }
-
-        public void thisIsDad()
-        {
-            this.parent2 = this.parent;
-            this.parent = null;
-        }
-
-        public Cat(String name, Cat parent, Cat parent2) {
-            this.name = name;
-            this.parent = parent;
-            this.parent2 = parent2;
+            this.father = father;
+            this.mother = mother;
         }
 
         @Override
         public String toString()
         {
-            if ((parent == null) && (parent2 == null))
-                return "Cat name is " + name + ", no mother" +  ", no father";
-            else if (parent == null)
-                return "Cat name is " + name + ", no mother, father is " + parent2.name;
-            else if (parent2 == null)
-                return "Cat name is " + name + ", mother is " + parent.name +", no father";
+            if ((mother == null) && (father == null))
+                return "Cat name is " + name + ", no mother, no father ";
+            else if (father == null)
+                return "Cat name is " + name + ", mother is " + mother.name + ", no father";
+            else if (mother == null)
+                return "Cat name is " + name + ", no mother, father is " + father.name;
             else
-                return "Cat name is " + name + ", mother is " + parent.name + ", father is " + parent2.name;
+                return "Cat name is " + name + ", mother is " + mother.name + ", father is " + father.name;
         }
     }
+
 }
